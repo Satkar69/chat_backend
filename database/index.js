2e64ffcc9b8f075ef73b6a55ae8f3757784b7c62
SatkarNiraula <satkarniraula69@gmail.com>
2024-09-06 19:39:33 +0545

    update :: docker containter for mongodb local database image  configured for development phase

--- database/index.js
+++ database/index.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
+// remote database
+// mongoose
+//   .connect(process.env.REMOTE_DATABASE_URI)
+//   .then(() => {
+//     console.log("Database connected successfully.");
+//   })
+//   .catch((error) => {
+//     console.log("Error connecting to the database: ", error);
+//   });
+
+// local database
 mongoose
-  .connect(process.env.DATABASE_URI)
+  .connect(process.env.LOCAL_DATABASE_URI)
   .then(() => {
     console.log("Database connected successfully.");
   })
