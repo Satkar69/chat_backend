c1ec4e9483ef313a2625787f2e698facf92ec254
SatkarNiraula <satkarniraula69@gmail.com>
2024-09-07 23:24:44 +0545

    update :: cors config updated

--- socket/socket.io.js
+++ socket/socket.io.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-frontend-seven-rho.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
